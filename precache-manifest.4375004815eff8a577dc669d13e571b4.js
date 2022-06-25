294dc48ed84167435bf5ecd40a699e84c79c4f36
santiagolavigna <santiagolavigna1@gmail.com>
2022-06-25 15:03:57 -0300

    Updates

---
+++ precache-manifest.4375004815eff8a577dc669d13e571b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "321ceee283e42d6a048b",
+    "url": "/s1web/static/css/main.f6021b01.chunk.css"
+  },
+  {
+    "revision": "321ceee283e42d6a048b",
+    "url": "/s1web/static/js/main.321ceee2.chunk.js"
+  },
+  {
+    "revision": "c663b2676af3e09beec7",
+    "url": "/s1web/static/css/1.506dcbdd.chunk.css"
+  },
+  {
+    "revision": "c663b2676af3e09beec7",
+    "url": "/s1web/static/js/1.c663b267.chunk.js"
+  },
+  {
+    "revision": "4e6e635d39efccc4d170",
+    "url": "/s1web/static/js/runtime~main.4e6e635d.js"
+  },
+  {
+    "revision": "4d7cfb583d713b8c0d68681e44911a7a",
+    "url": "/s1web/static/media/code.4d7cfb58.jpg"
+  },
+  {
+    "revision": "2386fe8a4c7b98216223d70550139617",
+    "url": "/s1web/index.html"
+  }
+];
