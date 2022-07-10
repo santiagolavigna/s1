7a2ec03146ebf3984b3bc4701918ceba71456240
santiagolavigna <santiagolavigna1@gmail.com>
2022-07-10 16:53:59 -0300

    About v3

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ export const About = () => {
   return (
     <div className='container-about'>
       <img src={require('/home/santiago/s1/master-react/s1-web/src/assets/img/s1.png')} alt='s1'></img>
-      <p className='p main-background'>
+      <p className='main-background'>
         I was born on march 4th, 1993 in Chivilcoy, Buenos Aires, Argentina. I have grown up with technology because my father is a hardware technician with a experience greater than 40 years, I worked with him by 7 years 
         where I got my first experiences in hardware and software, and while I was studying as well... until I graduated. So, today I am a semi senior web full stack developer, specializing in backend development. Experienced with all stages of the development cycle for dynamic web projects. Well-versed in numerous programming languages including native PHP OPP, Javascript/Jquery, MySQL, Frameworks like Laravel, Bootstrap, ReactJS, Template engines like Smarty, Symfony and complementary tools like Git, Jira, Docker, Figma, AWS and Prestashop (CMS).
       </p>
